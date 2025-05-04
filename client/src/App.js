105cd976ffb5c343c0cc3eb868192084d34b973a
kaipluxury <kyatutheekhai@gmail.com>
2025-05-04 07:03:08 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ProductList from './components/ProductList';
+import PaymentForm from './components/PaymentForm';
 
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to GrandX $tore</h1>
+      <ProductList />
+      <PaymentForm />
+    </div>
+  );
+}
+
+export default App;
