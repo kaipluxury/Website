98392d8c7d4c267f5fe5be222f51ba0117164ba7
kaipluxury <kyatutheekhai@gmail.com>
2025-05-04 06:59:42 +0530

    Update PaymentForm.js

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -1,1 +1,9 @@
 // Collects Discord username + proof of payment
+const products = [
+  "Trigger Bot (30 Days)",
+  "1337 Cheat (30 Days)",
+  "Custom Discord Bot",
+  "GrandRP BAN EVADE",
+  "Exclusive Leader / Admin Scripts",
+  "GrandRP Moneymethod"
+];
